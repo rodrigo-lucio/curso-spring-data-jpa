fd151cd195f0d21176095e2397492a1889a87488
Rodrigo Lúcio <luciodigo@gmail.com>
2021-09-21 22:13:56 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/alura/cursospringdatajpa/CursoSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.cursospringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringDataJpaApplication.class, args);
+	}
+
+}
