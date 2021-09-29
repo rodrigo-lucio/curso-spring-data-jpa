d0b3791d8f6a65b6e4a4591e29e21e7aacc3e6db
Rodrigo Lúcio <luciodigo@gmail.com>
2021-09-28 23:43:27 -0300

    Utilizado JPQL em uma consulta

--- src/main/java/br/com/alura/cursospringdatajpa/repository/FuncionarioRepository.java
+++ src/main/java/br/com/alura/cursospringdatajpa/repository/FuncionarioRepository.java
@@ -1,11 +1,14 @@
 package br.com.alura.cursospringdatajpa.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import br.com.alura.cursospringdatajpa.model.Cargo;
 import br.com.alura.cursospringdatajpa.model.Funcionario;
 
 @Repository
@@ -13,4 +16,6 @@ public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> 
 
 	Page<Funcionario> findByNomeContainingIgnoreCase(Pageable pageable, String nome);
 	
+	@Query("SELECT FROM Funcionario f WHERE f.nome = :nome AND f.salario >= :salario and f.dataContracacao = :dataContracacao")
+	List<Funcionario> findNomeDataContratacaoSalarioMario(String nome, Double salario, LocalDate dataContratacao);
 }
